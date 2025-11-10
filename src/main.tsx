4128366c674d4f905d04e7d4fc3dd5a1ca33040a
nicolasperezdeolaguer <nicolasperezdeo@gmail.com>
2025-11-10 17:14:33 +0100

    [Fix] Add basename and homepage in package.json

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles.css';
 
+const basename = process.env.PUBLIC_URL;
+
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+      <BrowserRouter basename={basename}>
+
   <React.StrictMode>
     <App />
   </React.StrictMode>
+            </BrowserRouter>
+
 );
